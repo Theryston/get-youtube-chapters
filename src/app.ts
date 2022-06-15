332f040a1596241f3c7336fa82ad165968044212
Theryston <funktodo2@gmail.com>
2022-06-15 08:42:23 -0300

    fix: use env

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { config } from "https://deno.land/std@0.143.0/dotenv/mod.ts";
 
 import { router } from "./routes/routes.ts";
 
-const HOST = config().API_HOST ?? "127.0.0.1";
-const PORT = config().API_PORT ?? 8080;
+const ENV = await config();
+
+const HOST = ENV.API_HOST ?? "127.0.0.1";
+const PORT = ENV.API_PORT ?? 8080;
 
 const app = new Application();
 
